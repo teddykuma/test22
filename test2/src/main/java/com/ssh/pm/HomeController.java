72ae0848cb7e6c20392cac4760fdec20b1452e12
soldesk <soldesk@soldesk-PC>
2019-02-26 13:06:51 +0900

    home.html

--- test2/src/main/java/com/ssh/pm/HomeController.java
+++ test2/src/main/java/com/ssh/pm/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		return "index";
+		return "home";
 	}
 	
 }
--- test2/src/main/webapp/WEB-INF/views/index.jsp
+++ test2/src/main/webapp/WEB-INF/views/index.jsp
@@ -7,6 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-	hi
+<table>
+<tr><td>배고프다..</td></tr>
+</table>
 </body>
 </html>
